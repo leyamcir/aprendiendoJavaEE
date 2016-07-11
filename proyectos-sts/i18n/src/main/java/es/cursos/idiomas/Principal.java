b88fd29aa1ba382ecc3e4a4edfbcfb37887fc966
Alicia Daza <amdazacastillo@gmail.com>
2016-07-11 12:50:20 +0200

    Locale with param examples

--- proyectos-sts/i18n/src/main/java/es/cursos/idiomas/Principal.java
+++ proyectos-sts/i18n/src/main/java/es/cursos/idiomas/Principal.java
@@ -14,14 +14,18 @@ ApplicationContext context;
 		// Cargar el contexto de Spring
 		context = new ClassPathXmlApplicationContext("applicationContext.xml");
 		
-		String pru = context.getMessage("mensaje", null, Locale.FRANCE);	
+		String pru = context.getMessage("mensaje", null, Locale.FRANCE);
+		String[] param = new String[] {"pepe"};
+		pru += "\n "+ context.getMessage("mostrarnombre", param, Locale.FRANCE);
 		System.out.println("FRANCE: " + pru);
 		
 		String pru2 = context.getMessage("mensaje", null, Locale.US);	
+		pru2 += "\n "+ context.getMessage("mostrarnombre", param, Locale.US);
 		System.out.println("ENGLISH: " + pru2);
 		
 		Locale esLocale = new Locale.Builder().setLanguage("es").setRegion("ES").build();
-		String pru3 = context.getMessage("mensaje", null, esLocale);	
+		String pru3 = context.getMessage("mensaje", null, esLocale);
+		pru3 += "\n "+ context.getMessage("mostrarnombre", param, esLocale);
 		System.out.println("ESPAÑOL: " + pru3);
 		
 		
