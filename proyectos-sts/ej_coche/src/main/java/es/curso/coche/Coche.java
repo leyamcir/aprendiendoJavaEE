d71b381dc9b06920d6ed411991e883a083ab0452
Alicia Daza <amdazacastillo@gmail.com>
2016-07-11 09:37:12 +0200

    Aplicación java con inyección de dependencias (de las dos formas permitidas)

---
+++ proyectos-sts/ej_coche/src/main/java/es/curso/coche/Coche.java
@@ -0,0 +1,49 @@
+package es.curso.coche;
+
+
+public class Coche {
+	
+	private Motor motor;
+	private Radio radio;
+	
+	// La clase Coche recibe las dependencias YA CREADAS
+	
+	// DOS formas de inyectar dependencias:
+	
+	// 1. Por constructor
+	public Coche(Motor motor, Radio radio) {
+		super();
+		this.motor = motor;
+		this.radio = radio;
+	}
+	
+	// 2. Por setter
+
+	public Coche() {
+		super();
+	}
+	
+
+	public Motor getMotor() {
+		return motor;
+	}
+
+	public void setMotor(Motor motor) {
+		this.motor = motor;
+	}
+
+	public Radio getRadio() {
+		return radio;
+	}
+
+	public void setRadio(Radio radio) {
+		this.radio = radio;
+	}
+
+	@Override
+	public String toString() {
+		return "Coche [motor=" + motor + ", radio=" + radio + "]";
+	}
+	
+	
+}
---
+++ proyectos-sts/ej_coche/src/main/java/es/curso/coche/Principal.java
@@ -0,0 +1,28 @@
+package es.curso.coche;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		
+ApplicationContext context;
+		
+		// Cargar el contexto de Spring
+		context = new ClassPathXmlApplicationContext("applicationContext.xml");
+		
+		// Recuperar un bean del contexto de Spring
+		Coche c1 = (Coche) context.getBean("cocheObj");
+		System.out.println(c1);
+		
+		// Otra forma de hacer el casting
+		Coche c2 = context.getBean("cocheObj2", Coche.class);
+		System.out.println(c2);
+		
+		// Cerrar el contexto
+		((ClassPathXmlApplicationContext)context).close();
+
+	}
+
+}
