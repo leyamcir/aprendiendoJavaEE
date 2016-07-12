548e8023127f2bfdbc211ad4a71a0cd46eddce4e
Alicia Daza <amdazacastillo@gmail.com>
2016-07-12 12:29:08 +0200

    AOP around

--- proyectos-sts/ej_aspectos/src/main/java/es/curso/concierto/Publico.java
+++ proyectos-sts/ej_aspectos/src/main/java/es/curso/concierto/Publico.java
@@ -1,5 +1,7 @@
 package es.curso.concierto;
 
+import org.aspectj.lang.ProceedingJoinPoint;
+
 public class Publico {
 
 	private void aplaudir(){
@@ -12,5 +14,28 @@ public class Publico {
 	
 	private void abuchear(){
 		System.out.println("Buuuuu, Fueraaaaa");
+	}
+	
+	public void verActuacion(ProceedingJoinPoint joinpoint) { 
+		try { 
+			// Tareas que queremos hacer antes del método: 
+			System.out.println("The audience is taking their seats."); 
+			System.out.println("The audience is turning off their cellphones"); 
+			long start = System.currentTimeMillis(); 
+			
+			joinpoint.proceed(); 
+			joinpoint.proceed(); 
+			joinpoint.proceed(); 
+			
+			// OJO, en este caso tenemos que lanzar la ejecución del método. 
+			// en este caso es el método perform(). 
+			long end = System.currentTimeMillis(); 
+			
+			// Tareas que queremos hacer después de la ejecución del método. 
+			System.out.println("CLAP CLAP CLAP CLAP CLAP"); 
+			System.out.println("The performance took " + (end - start) + " milliseconds."); 
+		} catch (Throwable t) { 
+			System.out.println("Boo! We want our money back!"); // Si hay error 
+		} 
 	}
 }
