d1e2ddd2854641e559a9fc7634361ec7aeabc637
Alicia Daza <amdazacastillo@gmail.com>
2016-07-13 08:59:53 +0200

    Query object (count rows)

--- proyectos-sts/jdbc_template/src/main/java/es/curso/bd/Principal.java
+++ proyectos-sts/jdbc_template/src/main/java/es/curso/bd/Principal.java
@@ -2,13 +2,30 @@ package es.curso.bd;
 
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.jdbc.core.JdbcTemplate;
 
 public class Principal {
 
 	public static void main(String[] args) {
 		ApplicationContext context;
+		JdbcTemplate template;
+		String sql;
+		int n;
 		
 		context = new ClassPathXmlApplicationContext("applicationContext.xml");
+		
+		// Get template from context
+		template = (JdbcTemplate) context.getBean("jdbcTemplate");
+		
+		// From here, queries are allowed using template
+		
+		// Count table rows
+		sql = "SELECT COUNT(*) FROM clientes";
+		// queryForInt is deprecated, use for Object instead
+		n = template.queryForObject(sql, Integer.class);
+		
+		System.out.println("Num clientes: " + n);
+		
 		
 		((ClassPathXmlApplicationContext) context).close();
 
