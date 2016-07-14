8b406b03931567d6fee2fae16a6e3ee3141b29aa
Alicia Daza <amdazacastillo@gmail.com>
2016-07-14 10:22:56 +0200

    Catch exceptions

--- proyectos-sts/aop_log_jdbc_tx1/src/main/java/es/curso/app/principal/Principal.java
+++ proyectos-sts/aop_log_jdbc_tx1/src/main/java/es/curso/app/principal/Principal.java
@@ -13,60 +13,63 @@ public class Principal {
 
 	public static void main(String[] args) {
 		ApplicationContext context;
-		
+
 		context = new ClassPathXmlApplicationContext("applicationContext.xml");
 		ITrabajadorBO bo = (ITrabajadorBO) context.getBean("trabajadorBO");
-		
+
 		// Probar los métodos
-		
-		// Insert
-		Trabajador ti = new Trabajador(0, "462", "Manuel", "Urgente");
-		int n = bo.saveBO(ti);
-		System.out.println(n);
-		
-		// Delete
-		int deleteTrabajador = 100;
-		int numDelete = bo.deleteBO(deleteTrabajador);
-		System.out.println("Borrado: "+numDelete);
-		
-		// SELECT ONE
-		int selectTrabajador = 97;
-		Trabajador t = bo.getBO(selectTrabajador);
-		System.out.println("Trabajador: "+t);
-		
-		// Select many
-		List<Trabajador> trabajadores = bo.getAllBO();
-		System.out.println("Trabajadores:");
-		for (Trabajador trabajador: trabajadores) {
-			System.out.println(trabajador);
-		}
-		
-		// COUNT ALL
-		int numt = bo.numTrabajadoresBO();
-		System.out.println("Num trabajadores: "+ numt);
-		
-		// Select by fields
-		// Departamento informatica
-		Trabajador tr = new Trabajador();
-		tr.setDepartamento("Informatica");
-		
-		List<Trabajador> trabajadores2 = bo.getAllBO(tr);
-		System.out.println("Trabajadores en informatica:");
-		for (Trabajador trabajador: trabajadores2) {
-			System.out.println(trabajador);
-		}
-		
-		// Nombre ant
-		Trabajador tr2 = new Trabajador();
-		tr2.setNombre("anto");
-		
-		List<Trabajador> trabajadores3 = bo.getAllBO(tr2);
-		System.out.println("Trabajadores con anton:");
-		for (Trabajador trabajador: trabajadores3) {
-			System.out.println(trabajador);
+		try{
+			// Insert
+			Trabajador ti = new Trabajador(0, "462", "Manuel", "Urgente");
+			int n = bo.saveBO(ti);
+			System.out.println(n);
+
+			// Delete
+			int deleteTrabajador = 100;
+			int numDelete = bo.deleteBO(deleteTrabajador);
+			System.out.println("Borrado: "+numDelete);
+
+			// SELECT ONE
+			int selectTrabajador = 97;
+			Trabajador t = bo.getBO(selectTrabajador);
+			System.out.println("Trabajador: "+t);
+
+			// Select many
+			List<Trabajador> trabajadores = bo.getAllBO();
+			System.out.println("Trabajadores:");
+			for (Trabajador trabajador: trabajadores) {
+				System.out.println(trabajador);
+			}
+
+			// COUNT ALL
+			int numt = bo.numTrabajadoresBO();
+			System.out.println("Num trabajadores: "+ numt);
+
+			// Select by fields
+			// Departamento informatica
+			Trabajador tr = new Trabajador();
+			tr.setDepartamento("Informatica");
+
+			List<Trabajador> trabajadores2 = bo.getAllBO(tr);
+			System.out.println("Trabajadores en informatica:");
+			for (Trabajador trabajador: trabajadores2) {
+				System.out.println(trabajador);
+			}
+
+			// Nombre ant
+			Trabajador tr2 = new Trabajador();
+			tr2.setNombre("anto");
+
+			List<Trabajador> trabajadores3 = bo.getAllBO(tr2);
+			System.out.println("Trabajadores con anton:");
+			for (Trabajador trabajador: trabajadores3) {
+				System.out.println(trabajador);
+			}
+
+		} catch(RuntimeException e) {
+			System.out.println(e.getMessage());
 		}
 
-		
 		((ClassPathXmlApplicationContext) context).close();
 
 	}
--- proyectos-sts/aop_log_jdbc_tx2_xml/src/main/java/es/curso/app/principal/Principal.java
+++ proyectos-sts/aop_log_jdbc_tx2_xml/src/main/java/es/curso/app/principal/Principal.java
@@ -18,53 +18,58 @@ public class Principal {
 		ITrabajadorBO bo = (ITrabajadorBO) context.getBean("trabajadorBO");
 		
 		// Probar los métodos
-		
-		// Insert
-		Trabajador ti = new Trabajador(0, "466", "Carla", "Recursos Humanos");
-		int n = bo.saveBO(ti);
-		System.out.println(n);
-		
-		// Delete
-		int deleteTrabajador = 105;
-		int numDelete = bo.deleteBO(deleteTrabajador);
-		System.out.println("Borrado: "+numDelete);
-		
-		// SELECT ONE
-		int selectTrabajador = 97;
-		Trabajador t = bo.getBO(selectTrabajador);
-		System.out.println("Trabajador: "+t);
-		
-		// Select many
-		List<Trabajador> trabajadores = bo.getAllBO();
-		System.out.println("Trabajadores:");
-		for (Trabajador trabajador: trabajadores) {
-			System.out.println(trabajador);
+		try {
+			// Insert
+			Trabajador ti = new Trabajador(0, "466", "Carla", "Recursos Humanos");
+			int n = bo.saveBO(ti);
+			System.out.println(n);
+			
+			// Delete
+			int deleteTrabajador = 105;
+			int numDelete = bo.deleteBO(deleteTrabajador);
+			System.out.println("Borrado: "+numDelete);
+			
+			// SELECT ONE
+			int selectTrabajador = 97;
+			Trabajador t = bo.getBO(selectTrabajador);
+			System.out.println("Trabajador: "+t);
+			
+			// Select many
+			List<Trabajador> trabajadores = bo.getAllBO();
+			System.out.println("Trabajadores:");
+			for (Trabajador trabajador: trabajadores) {
+				System.out.println(trabajador);
+			}
+			
+			// COUNT ALL
+			int numt = bo.numTrabajadoresBO();
+			System.out.println("Num trabajadores: "+ numt);
+			
+			// Select by fields
+			// Departamento informatica
+			Trabajador tr = new Trabajador();
+			tr.setDepartamento("Informatica");
+			
+			List<Trabajador> trabajadores2 = bo.getAllBO(tr);
+			System.out.println("Trabajadores en informatica:");
+			for (Trabajador trabajador: trabajadores2) {
+				System.out.println(trabajador);
+			}
+			
+			// Nombre ant
+			Trabajador tr2 = new Trabajador();
+			tr2.setNombre("anto");
+			
+			List<Trabajador> trabajadores3 = bo.getAllBO(tr2);
+			System.out.println("Trabajadores con anton:");
+			for (Trabajador trabajador: trabajadores3) {
+				System.out.println(trabajador);
+			}
+		} catch(RuntimeException e) {
+			System.out.println(e.getMessage());
 		}
 		
-		// COUNT ALL
-		int numt = bo.numTrabajadoresBO();
-		System.out.println("Num trabajadores: "+ numt);
 		
-		// Select by fields
-		// Departamento informatica
-		Trabajador tr = new Trabajador();
-		tr.setDepartamento("Informatica");
-		
-		List<Trabajador> trabajadores2 = bo.getAllBO(tr);
-		System.out.println("Trabajadores en informatica:");
-		for (Trabajador trabajador: trabajadores2) {
-			System.out.println(trabajador);
-		}
-		
-		// Nombre ant
-		Trabajador tr2 = new Trabajador();
-		tr2.setNombre("anto");
-		
-		List<Trabajador> trabajadores3 = bo.getAllBO(tr2);
-		System.out.println("Trabajadores con anton:");
-		for (Trabajador trabajador: trabajadores3) {
-			System.out.println(trabajador);
-		}
 
 		
 		((ClassPathXmlApplicationContext) context).close();
